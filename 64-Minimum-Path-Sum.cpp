67314118c2f46c37c99737bbb08180d677226d94
DEVESH KR PATHAK <pathakdevesh165@gmail.com>
2024-09-20 22:54:36 +0530

    Update 64-Minimum-Path-Sum.cpp

--- 64-Minimum-Path-Sum.cpp
+++ 64-Minimum-Path-Sum.cpp
@@ -21,4 +21,46 @@ int help(vector<vector<int>>& grid,int i,int j)
           dp = vector<vector<int>>(m, vector<int>(n, -1));
         return help(grid,grid.size()-1,grid[0].size()-1);
     }
+};//////
+
+
+
+
+
+//tabulation approchh
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        // Initialize dp array
+        dp = vector<vector<int>>(m, vector<int>(n, -1));
+        
+        // Base case: top-left corner
+        dp[0][0] = grid[0][0];
+        
+        // Fill the first row (can only come from the left)
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        
+        // Fill the first column (can only come from above)
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        
+        // Fill the rest of the dp table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        // Return the minimum path sum at the bottom-right corner
+        return dp[m-1][n-1];
+    }
 };
